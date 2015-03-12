cee8878d960b2ef2cf358ecb8ea085882862ebf1
Zeroth99 <alexfzbrito@gmail.com>
2015-03-12 14:41:20 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("http://i.imgur.com/SBAso1N.jpg");
+                    API.sendChat("/me http://i.imgur.com/SBAso1N.jpg");
                 }
             }
         };
