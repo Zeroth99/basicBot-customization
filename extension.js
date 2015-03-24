dba146d394f3d7ccd322e02d6bb0422cc5ce314e
Zeroth99 <alexfzbrito@gmail.com>
2015-03-24 02:04:38 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -30,6 +30,19 @@
          }
          }
          
+         bot.commands.tpCommand = {
+         command: 'tp',
+         rank: 'user',
+         type: 'exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+            API.sendChat("/me To be able to see twitch emotes download this plugin: https://fungustime.pw/tastyplug/");
+         }
+         }
+         }
+         
          bot.commands.howtodjCommand = {
          command: 'howtodj',
          rank: 'user',
